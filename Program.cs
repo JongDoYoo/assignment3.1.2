140e6d6f2915238d40f609e52a94d20313993a62
JongD <JongD@JONGDO>
2024-05-21 06:10:04 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace assignment3._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year to check if its Leap year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year");
+            }
+        }
+    }
+}
